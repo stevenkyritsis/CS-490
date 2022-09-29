5d07aa42d0c8de39c6788ada2ba22bd75598aacf
Steven Kyritsis <16929888+stevenkyritsis@users.noreply.github.com>
2022-09-29 07:04:59 -1000

    uppi

--- login.js
+++ login.js
@@ -1,1 +1,8 @@
-let 
+//User validation
+var email = document.forms['form']['uname'];
+var passwd = document.forms['form']['passwd'];
+
+var email_error = document.getElementById('uname_error');
+var passwd_error = document.getElementById('passwd_error');
+
+function validated()
