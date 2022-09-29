5a19360694b278849415a8808f9a59eea61b4b54
Steven Kyritsis <16929888+stevenkyritsis@users.noreply.github.com>
2022-09-29 11:04:11 -1000

    almost

--- login.js
+++ login.js
@@ -5,4 +5,26 @@ var passwd = document.forms['form']['passwd'];
 var email_error = document.getElementById('uname_error');
 var passwd_error = document.getElementById('passwd_error');
 
-function validated()
+function validated(){
+    let name = document.forms["form"]["uname"].value;
+    let pass = document.forms["form"]["passwd"].value;
+    if (name == "") {
+        alert("Username must be filled out");
+        return false;
+    }
+    
+    if (name == "student" && pass == "password"){
+        location.replace("/student.html");
+        //return true;
+    }
+    else if (name == "teacher" && pass == "password"){
+        location.replace("/teacher.html");
+        //return true;
+        log("true");
+        console("message");
+    }
+    else{
+        alert("Error! Incorrect username or password!");
+        return false;
+    }
+}
