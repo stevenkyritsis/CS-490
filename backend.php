0ca496cd70c2227c74c9e1e02d0aef8cb4f7a9bd
Steven Kyritsis <16929888+stevenkyritsis@users.noreply.github.com>
2022-10-11 15:31:44 -0400

    Update backend.php

--- backend.php
+++ backend.php
@@ -1,7 +1,6 @@
 <?php
 
 $data = $_POST;
-
 $host = "sql2.njit.edu";
 $user =  "sak76";
 $table  = "users";
@@ -18,47 +17,16 @@ mysqli_select_db($db,$table);
 
 $username = $data['username'];
 $password = $data['password'];
-$sql = "SELECT * FROM 'users' WHERE UNAME = '$username' AND PASSWD = '$password'";
+$sql = "SELECT * FROM users WHERE UNAME = '$username' AND PASSWD = '$password'";
 $result = mysqli_query ($db,$sql);
-
+if(!($result)){
+  echo mysqli_error($db);
+}
 $row = mysqli_fetch_array($result);
+
 mysqli_close($db);
 if($row['PASSWD']!=NULL)
     $hash_pass = password_hash($row['PASSWD'],1);
 $result_arr = array('username' => $row['UNAME'], 'password' => $hash_pass, 'role' => $row['ROLE']);
-echo (json_encode($result_arr));
-
-/*
-$s = "SELECT * FROM 'users' WHERE USER ='$username'";
-$t = mysqli_query($db,$s);
-$row = mysqli_fetch_row($t);
-$thepassword = $row[1];
-*/
-/*
-//password hash for later
-$hash = password_hash($passwd, PASSWORD_DEFAULT);
-if(password_verify($thepassword, $hash)){
-    $response = 'true';
-  }
-else $response = 'false';
-
-if ($thepassword == ""){
-  $response = 'false';
-}
-
-//echo $response;
-
-if ($response == 'true'){
-	$q = "SELECT ROLE from USER INFO where USER  ='$username'";
-	$w = mysqli_query($db,$q);
-	$rowe = mysqli_fetch_row($w);
-	$role = $rowe[2];
-	echo $role;
-}
-
-mysqli_free_result($t);
-mysqli_free_result($w);
-mysqli_close($db);
-exit();
-*/
+echo (json_encode($result_arr)); 
 ?>
