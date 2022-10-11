e7c72a2bd8bc53e3c2cb563ca354302249e7dec6
stevenkyritsis <16929888+stevenkyritsis@users.noreply.github.com>
2022-10-11 00:34:51 -0400

    Update backend.php

--- backend.php
+++ backend.php
@@ -2,31 +2,47 @@
 
 $recieved = json_decode(file_get_contents("php://input"), true);
 
-$host = "sql1.njit.edu";
+$host = "sql2.njit.edu";
 $user =  "sak76";
 $table  = "users";
 $pwd = "##Tessy12345";
 
-$db = mysqli_connect($host,$user, $pwd, $table);
-
-//$db = mysqli_connect($hostname,$user, $pwd);
+$db = mysqli_connect($host,$user, $pwd);
 
 if (mysqli_connect_errno())
   {	  
     echo "FAILED CONNECTION: " . mysqli_connect_error();
   }
 
-mysqli_select_db($db, $table); 
+mysqli_select_db($db,$table); 
 
-$username = $_POST [ "username" ] ;
-$password = $_POST [ "password" ] ;
+$username = $_POST['user'];
+$password = $_POST['passwd'];
 
-$s = "SELECT passwd from USER INFO where USER ='$username'";
+$result = mysqli_query ($db,"SELECT * FROM 'users'
+	WHERE 'USER' LIKE '$username'
+	AND PASSWD LIKE '$password'
+	");
+
+if ($row = mysqli_fetch_array($result)) {
+
+  do {
+    print $row["USER"];
+    print (" ");
+    print $row["PASSWD"];
+    print("<p>");
+  } while($row = mysql_fetch_array($result));
+  } else {print "Sorry, no records were found!";
+  }
+/*
+$s = "SELECT * FROM 'users' WHERE USER ='$username'";
 $t = mysqli_query($db,$s);
 $row = mysqli_fetch_row($t);
 $thepassword = $row[1];
-
-$hash = password_hash($password, PASSWORD_DEFAULT);
+*/
+/*
+//password hash for later
+$hash = password_hash($passwd, PASSWORD_DEFAULT);
 if(password_verify($thepassword, $hash)){
     $response = 'true';
   }
@@ -50,5 +66,5 @@ mysqli_free_result($t);
 mysqli_free_result($w);
 mysqli_close($db);
 exit();
-
+*/
 ?>
