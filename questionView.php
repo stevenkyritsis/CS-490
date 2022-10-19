3bb3e069651f2d28b6d4c340d3058a54e4ca5136
Steven Kyritsis <16929888+stevenkyritsis@users.noreply.github.com>
2022-10-19 00:22:53 -0400

    Update questionView.php

--- questionView.php
+++ questionView.php
@@ -21,4 +21,22 @@ if (mysqli_connect_errno())
 
 mysqli_select_db($db,$table); 
 
+$sql = "SELECT * FROM $table";
+
+$result = mysqli_query ($db,$sql);
+if(!($result)){
+  echo mysqli_error($db);
+}
+//$row = mysqli_fetch_array($result);
+
+while ($row = mysqli_fetch_assoc($result)) { // Important line !!! Check summary get row on array ..
+  foreach ($row as $field => $value) { // I you want you can right this line like this: foreach($row as $value) {
+      echo $value; // I just did not use "htmlspecialchars()" function. 
+  }
+}
+mysqli_close($db);
+//$result_arr = array('question' => $row['question'], 'answer' => $row['answer']);
+
+//echo(json_encode($result_arr));
+
 ?>
