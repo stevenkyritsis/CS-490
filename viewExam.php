d6569c753b0b1dab0fcf08a15a5db52f2306a1af
Steven Kyritsis <16929888+stevenkyritsis@users.noreply.github.com>
2022-11-22 17:17:41 -0500

    viewExam now pulls all questions associated with the exam

--- viewExam.php
+++ viewExam.php
@@ -2,7 +2,8 @@
 
 $host = "sql2.njit.edu";
 $user =  "sak76";
-$table  = "new_exam";
+$table1  = "new_exam";
+$table2 = "question";
 $pwd = "##Tessy12345";
 $db = mysqli_connect($host,$user, $pwd, $user);
 
@@ -19,12 +20,14 @@ if (mysqli_connect_errno())
 
 mysqli_select_db($db,$table); 
 
-$sql = "SELECT * FROM $table WHERE exam_id = '$exam_id'";
-$result = mysqli_query ($db,$sql);
+$sql = "SELECT question.questionid, exam_id, question.question, grade FROM $table1, $table2 WHERE exam_id = '$exam_id' AND $table1.questionid = $table2.questionid";
+//$result = mysqli_query($db,$sql);
+$result = $db->query($sql) or die($db->error);
 
 while($row = $result->fetch_assoc()) {
     $result_arr[] = array('exam_id' => $row['exam_id'],
        'question_id' => $row['questionid'],
+       'question' => $row['question'],
        'grade' => $row['grade']);  
   }
 
