0acc719b110bb5f207e3f4172b887743766e0577
Steven Kyritsis <16929888+stevenkyritsis@users.noreply.github.com>
2022-11-22 17:16:51 -0500

    autograder is still a WIP

--- auto.php
+++ auto.php
@@ -4,15 +4,19 @@ $host = "sql2.njit.edu";
 $user =  "sak76";
 $table1  = "answers";
 $table2 = "new_exam";
+$table3 = "question";
+$table4 = "exam_status";
 $pwd = "##Tessy12345";
 
 $db = mysqli_connect($host,$user, $pwd, $user);
 
 $indata = file_get_contents('php://input');
 
-$user = $indata->$username;
-$exam_num = $indata->exam_id;
-$questions = $indata->questions;
+$data = json_decode($indata);
+
+$user = $data->username;
+$exam_num = $data->exam_id;
+$questions[] = $data->questions;
 
 if (mysqli_connect_errno())
   {	  
@@ -21,7 +25,17 @@ if (mysqli_connect_errno())
 
 mysqli_select_db($db,$table); 
 
-$sql = "SELECT * FROM $table";
+$status_sql = "SELECT  exam_status.exam_id,
+                exam_status.STATUS,
+                exam_status.student_id
+
+                 FROM $table1, $table2, $table3 
+                 WHERE exam_status.examid = $exam_num
+                 AND exam_status.student_id = $user
+                 AND exam_status.STATUS = 'SUBMITTED'
+                 ";
+
+$answers_sql = 
 
 $result = mysqli_query($db,$sql);
 
