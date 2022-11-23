e50854311ab08575bc2acc06285d7e31538dd0fd
Steven Kyritsis <16929888+stevenkyritsis@users.noreply.github.com>
2022-11-22 22:35:51 -0500

    Update auto.php

--- auto.php
+++ auto.php
@@ -3,9 +3,8 @@
 $host = "sql2.njit.edu";
 $user =  "sak76";
 $table1  = "answers";
-$table2 = "new_exam";
-$table3 = "question";
-$table4 = "exam_status";
+$table2 = "question";
+$table3 = "exam_status";
 $pwd = "##Tessy12345";
 
 $db = mysqli_connect($host,$user, $pwd, $user);
@@ -25,17 +24,10 @@ if (mysqli_connect_errno())
 
 mysqli_select_db($db,$table); 
 
-$status_sql = "SELECT  exam_status.exam_id,
-                exam_status.STATUS,
-                exam_status.student_id
+$answers_sql = "SELECT $table1. FROM $table1, $table2 WHERE '$table1'.exam_id = $exam_num
+                        AND '$table1'.student_id = '$user'";
 
-                 FROM $table1, $table2, $table3 
-                 WHERE exam_status.examid = $exam_num
-                 AND exam_status.student_id = $user
-                 AND exam_status.STATUS = 'SUBMITTED'
-                 ";
 
-$answers_sql = 
 
 $result = mysqli_query($db,$sql);
 
